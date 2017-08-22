79bff8466d564c0bf93c133a32501fb1d6ee5eed
Leó Stefánsson <leostefansson@gmail.com>
2017-08-22 11:16:26 -0700

    removed a lot of unneccessary code.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -77,7 +77,14 @@ void ofApp::update(){
         ofxCv::blur(thresholded, 10);
         contourFinder.findContours(thresholded);
         // TODO: Turn off LED here
+        for(int i = 0; i < contourFinder.size(); i++) {
+            // Store centroids for drawing/saving
+            ofPoint center = ofxCv::toOf(contourFinder.getCenter(i));
+            centroids.push_back(center);
+        }
     }
+    
+    ofSetColor(ofColor::white);
 }
 
 //--------------------------------------------------------------
@@ -93,55 +100,15 @@ void ofApp::draw(){
     
     ofSetColor(0, 255, 0);
     //movie.draw(0, 0);
-    contourFinder.draw();
-    for(int i = 0; i < contourFinder.size(); i++) {
-        ofPoint center = ofxCv::toOf(contourFinder.getCenter(i));
-        // Store centroids for drawing/saving
-        centroids.push_back(center);
-        ofPushMatrix();
-        ofTranslate(center.x, center.y);
-        int label = contourFinder.getLabel(i);
-        string msg = ofToString(label) + ":" + ofToString(tracker.getAge(label));
-        //ofDrawBitmapString(msg, 0, 0);
-        ofVec2f velocity = ofxCv::toOf(contourFinder.getVelocity(i));
-        ofScale(5, 5);
-        ofDrawLine(0, 0, velocity.x, velocity.y);
-        ofPopMatrix();
-        
-            
-    }
+    contourFinder.draw(); // Draws the blob rect surrounding the contour
     
+    // Draw the detected contour center points
     for (int i = 0; i < centroids.size(); i++) {
         ofDrawCircle(centroids[i].x, centroids[i].y, 3);
     }
     
-    // this chunk of code visualizes the creation and destruction of labels
-    const vector<unsigned int>& currentLabels = tracker.getCurrentLabels();
-    const vector<unsigned int>& previousLabels = tracker.getPreviousLabels();
-    const vector<unsigned int>& newLabels = tracker.getNewLabels();
-    const vector<unsigned int>& deadLabels = tracker.getDeadLabels();
-    ofSetColor(ofxCv::cyanPrint);
-    for(int i = 0; i < currentLabels.size(); i++) {
-        int j = currentLabels[i];
-        ofDrawLine(j, 0, j, 4);
-    }
-    ofSetColor(ofxCv::magentaPrint);
-    for(int i = 0; i < previousLabels.size(); i++) {
-        int j = previousLabels[i];
-        ofDrawLine(j, 4, j, 8);
-    }
-    ofSetColor(ofxCv::yellowPrint);
-    for(int i = 0; i < newLabels.size(); i++) {
-        int j = newLabels[i];
-        ofDrawLine(j, 8, j, 12);
-    }
-    ofSetColor(ofColor::white);
-    for(int i = 0; i < deadLabels.size(); i++) {
-        int j = deadLabels[i];
-        ofDrawLine(j, 12, j, 16);
-    }
     
-    
+
 }
 
 //--------------------------------------------------------------
